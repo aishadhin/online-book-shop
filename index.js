34ec48d415cf05066b5a411a4e27a12b20f1402b
Ai Shadhin <aishadhin.ac@gmail.com>
2022-07-25 22:20:58 +0600

    make it better

--- index.js
+++ index.js
@@ -2,13 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 app.use(cors());
 app.use(express.json());
 
 
-app.get('/',(req, res)=>{
+
+
+
+const uri = "mongodb+srv://OnlineBookStore:suD5wAadFKukGfoR@cluster0.u5nmk.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const bookCollections = client.db("BookStoreDatabase").collection("book-collections");
+        
+        app.get('/products', async(req,res)=>{
+            const query = {};
+            const cursor = bookCollections.find(query);
+            const books = await cursor.toArray();
+            res.send(books)
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
     res.send('kamal hain')
 })
 
